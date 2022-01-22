20ed6399810a63a2eeafc740e78b099402067ad1
Adam Geenen <adamgeenen@gmail.com>
2022-01-22 18:23:56 -0500

    Able to text number and print incoming message

--- send_sms.js
+++ send_sms.js
@@ -1,14 +1,14 @@
 // Download the helper library from https://www.twilio.com/docs/node/install
 // Find your Account SID and Auth Token at twilio.com/console
 // and set the environment variables. See http://twil.io/secure
-const accountSid = process.env.TWILIO_ACCOUNT_SID;
-const authToken = process.env.TWILIO_AUTH_TOKEN;
+const accountSid = "AC0cc3256161ea3a8329cbd721e9f38b7f";
+const authToken = "2997c232e961810ab87ccec15e1f1774";
 const client = require('twilio')(accountSid, authToken);
 
 client.messages
     .create({
-        body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
-        from: '+15017122661',
-        to: '+15558675310'
+        body: 'Hello sir, Is this working?>?',
+        from: '+16203178894',
+        to: '+15144529399'
     })
     .then(message => console.log(message.sid));
---
+++ server.js
@@ -0,0 +1,23 @@
+const http = require('http');
+const express = require('express');
+const MessagingResponse = require('twilio').twiml.MessagingResponse;
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.post('/sms', (req, res) => {
+    console.log(req.body.Body);
+
+  const twiml = new MessagingResponse();
+
+  twiml.message('Hello Adam, I have recieved your message!');
+
+  res.writeHead(200, {'Content-Type': 'text/xml'});
+  res.end(twiml.toString());
+});
+
+http.createServer(app).listen(1337, () => {
+  console.log('Express server listening on port 1337');
+});
